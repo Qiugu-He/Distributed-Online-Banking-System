c673d7d99b0fe732ad173ad7ead60707f04d7146
Qiugu-He <44444334+Qiugu-He@users.noreply.github.com>
2021-10-18 22:10:10 -0500

    Update MultiThreaded_Server.java

--- MultiThreaded_Server.java
+++ MultiThreaded_Server.java
@@ -11,6 +11,7 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import java.sql.*;
 
 public class SevMT{
 
@@ -19,8 +20,37 @@ public class SevMT{
 	ServerSocket sock = null;    // server's master socket
 	InetAddress addr = null;     // address of server
 	Socket cli = null;           // client socket returned from accept	
-	System.out.println("Server starting.");
+	String DBName = null ;
+	String userID = null;
+	String userPw = "";
 
+	if(args.length == 3)
+	{
+		DBName = args[0];
+		userID =args[1];
+		userPw = args[2];
+	}
+	else if(args.length == 2)
+	{	
+		boolean gotUps = false;
+		while(!gotUps)
+		{
+			System.out.println("Please provide your user password: ");
+			Scanner scan = new Scanner (System.in);
+			userPw = scan.nextLine().trim();
+			if(!userPw.equals(""))
+			{
+				gotUps = true;
+			}
+		}//while
+	}
+	else
+	{
+		System.out.println("Please run again with correct DBname, userID and user userPw: ");
+		System.exit(0);
+	}
+
+	System.out.println("Server starting.");
 	// Create main ServerSocket
 	try {
 	    addr = InetAddress.getLocalHost();
@@ -42,7 +72,7 @@ public class SevMT{
 	    } // end try-catch
 	    
 	    // Create a thread for this client (which starts our run() method
-	    new Thread(new myThread(cli)).start();
+	    new Thread(new myThread(cli, DBName, userID, userPw)).start();
 	} // end while - accept
 
 	// Will never get here - its a server!
@@ -56,19 +86,58 @@ public class SevMT{
 class myThread implements Runnable {
 
     Socket cliSock = null;  // socket for each client
-    // *** This is instantiated per client whenever
-    // a new Thread is created
-
-   private static Hashtable <String, Integer>table = new Hashtable<String, Integer>();
-
-    myThread(Socket csocket) {// constructor called by server for each client
+							// *** This is instantiated per client whenever
+							// a new Thread is create
+    String DBName;
+    String userID;
+    String userPw;
+    myThread(Socket csocket, String DBName, String userID, String userPw) {// constructor called by server for each client
 	this.cliSock=csocket;
+	this.DBName = DBName;
+	this.userID = userID;
+	this.userPw = userPw;
     } // end constructor
 
     public void run() {
     	InputStreamReader strm = null;
 		BufferedReader strmIn = null;
 		OutputStream out = null;
+
+
+		 /* --------------------------------
+			Make connection to JDBC
+		-------------------------------------*/
+		// First get a JDBC driver, in this case for a mySQL/MariaDB database
+		System.out.println("Client accessing database... ");
+		try {
+	    	Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) {
+			System.out.println("Couldn't get the needed JDBC driver");
+			e.printStackTrace();
+			System.exit(44);
+		} // end try-catch
+		// Next, get a connection to the database
+		Connection con=null;
+		try {
+			//String srvr = "silicon.cs.umanitoba.ca";
+	    	String srvr = "127.0.0.1";
+	    	String port = "3306";
+	    	// construct URL address for database
+	    	String db = DBName;
+	    	String url = "jdbc:mysql://"+srvr+":"+port+"/"+db;
+		
+	    	String uid = userID;
+	    	String pw = userPw;
+	    	
+	    	// make connection to database
+	    	con = DriverManager.getConnection(url, uid, pw);
+		} catch( Exception e ) {
+			System.out.println("Connection failed");
+	    	e.printStackTrace();
+			System.exit(55);
+		} // end try-catch
+		System.out.println("Client connected to database successfully. ");
+
 
     	//read stream from Client
     	try{
@@ -96,7 +165,7 @@ class myThread implements Runnable {
 
     				reqSplit = request.split("<|,|>");
 
-    				processOperation(request, reqSplit, out, close);
+    				processOperation(request, reqSplit, out, close, con);
 
     			}//while
     		}catch (Exception e) {
@@ -120,7 +189,7 @@ class myThread implements Runnable {
     }// end run 
 
 
-     //simply check is the input string is integer
+    //simply check is the input string is integer
     public static boolean isInteger(String s) {
     return isInteger(s,10);
 	}//end isInteger
@@ -146,70 +215,40 @@ class myThread implements Runnable {
 // Remark: this methods process the request operation from 
 //		   client and send the response message back to client
 //-----------------------------------------------
-	public static synchronized boolean processOperation(String request, String[] reqSplit, OutputStream out, boolean close)
+	public static synchronized boolean processOperation(String request, String[] reqSplit, OutputStream out, boolean close, Connection con)
 	{	
 		boolean toReturn = close;
 		String ecoMsg = "";
 
 		try{
-			 //Command E
+			 //------------ E ------------
 	      	if(request.equals("E"))
 	      	{
-	      		synchronized(myThread.class)
-	      		{
 				close = true;
 				ecoMsg = "Client Terminated. ";
 	      		out.write(ecoMsg.getBytes());
-      			}
 	      	}
-	      	//Command C and R
 	      	else if(reqSplit.length == 2)
 	      	{	
 	      		boolean check = isInteger(reqSplit[1]);
 	      		if(check)
 	      		{	
-	      			synchronized(myThread.class)
-	      			{
-	      			//C Command
+	      			//---------------C----------------------
 		      		if(reqSplit[0].equals("C"))
 		      		{
-		      			//ecoMsg = "Creating Account. ";
-		      			//out.write(ecoMsg.getBytes());
-
-		      			//seatch from table check if it existed
-		      			if(table.containsKey(reqSplit[1]))
-		      			{
-		      				ecoMsg = "Creation Failed, account number is already exist. ";
-		      				out.write(ecoMsg.getBytes());
-		      			}
-		      			else
-		      			{	
-		      				table.put(reqSplit[1], 0);
-		      				ecoMsg = "Creating Account " +reqSplit[1] + " Successfully. Balance: "+ table.get(reqSplit[1]) ;
-		      				out.write(ecoMsg.getBytes());
-		      			}
+		      			CreateAcc(reqSplit,con, out);
 		      		}
 
-		      		//R Command
+		      		//----------------R------------------------
 		      		else if(reqSplit[0].equals("R"))
 		      		{
-		      			if(table.containsKey(reqSplit[1]))
-		      			{
-		      				ecoMsg = "Retrieving Account " +reqSplit[1]+" successfully. Balance: "+ table.get(reqSplit[1]) + " ";
-		      				out.write(ecoMsg.getBytes());
-		      			}
-		      			else
-		      			{
-		      				ecoMsg = "Retrieving Failed. Account "+ reqSplit[1] + " does not existed.";
-		      				out.write(ecoMsg.getBytes());
-		      			}
+		      			RetrivelAcc(reqSplit, con, out);
 		      		}
 		      		else
 		      		{
 		      			ecoMsg =  "Invalid command format, plase enter again.";
 		      			out.write(ecoMsg.getBytes());
 		      		}
-		      	}
 		      	}
 		      	else
 		      	{
@@ -225,46 +264,29 @@ class myThread implements Runnable {
 	      		boolean check2 = isInteger(reqSplit[2]);
 	      		if(check && check2)
 	      		{	
-	      			synchronized(myThread.class)
-	      			{
-	      			if(reqSplit[0].equals("D") && !reqSplit[2].equals("0"))
-	      			{
-		      			if(table.containsKey(reqSplit[1]))
-		      			{
-		      				int val = table.get(reqSplit[1]);
-		      				table.put(reqSplit[1],val + Integer.parseInt(reqSplit[2]));
-		      				ecoMsg = "Deposit to Account: "+ reqSplit[1]+ " Successfully. New balance: "+ table.get(reqSplit[1]);
-		      				out.write(ecoMsg.getBytes());
+      				//--------------------D--------------------------
+	      			if(reqSplit[0].equals("D") )
+	      			{	
+	      				if(!reqSplit[2].equals("0"))
+	      				{
+		      				DepositToAcc(reqSplit,con, out);
 		      			}
 		      			else
 		      			{
-		      				ecoMsg = "Deposit Failed, Account " + reqSplit[1] + " does not exist.";
+		      				ecoMsg =  "Do you really want to deposit 0 to your account?";
 		      				out.write(ecoMsg.getBytes());
 		      			}
 	      			}
-	      			else if(reqSplit[0].equals("W") && !reqSplit[2].equals("0"))
-	      			{
-	      				//ecoMsg = "Withdraw from Account: "+ reqSplit[1] + " with amount "+reqSplit[2];
-		      			//out.write(ecoMsg.getBytes());
-		      			if(table.containsKey(reqSplit[1]))
+	      			//--------------------W-------------------------
+	      			else if(reqSplit[0].equals("W"))
+	      			{	
+	      				if(!reqSplit[2].equals("0"))
+	      				{
+	      					WdFromAcc(reqSplit, con, out);
+	      				}
+	      				else
 		      			{
-		      				int val = table.get(reqSplit[1]);
-		      				if(Integer.parseInt(reqSplit[2]) > val)
-		      				{
-		      					ecoMsg = "Withdraw from account: " + reqSplit[1]+" failed, does not enough money. Current blanace: "+ table.get(reqSplit[1]);
-		      					out.write(ecoMsg.getBytes());
-		      				}
-		      				else
-		      				{
-		      					table.put(reqSplit[1],val-Integer.parseInt(reqSplit[2]) );
-		      					ecoMsg = "Withdraw from account: " + reqSplit[1]+ " successfully. New balance: "+ table.get(reqSplit[1]);
-		      					out.write(ecoMsg.getBytes());
-		      				}
-		      				
-		      			}
-		      			else
-		      			{
-		      				ecoMsg = "Withdraw Faile, Account " + reqSplit[1] + " does not exist.";
+		      				ecoMsg =  "I really want to draw more money for you instead of 0..";
 		      				out.write(ecoMsg.getBytes());
 		      			}
 	      			}
@@ -273,8 +295,6 @@ class myThread implements Runnable {
 	      				ecoMsg =  "Invalid command format, plase enter again.";
 		      			out.write(ecoMsg.getBytes());
 	      			}
-	      			}
-
 	      		}
 	      		else
 	      		{
@@ -303,4 +323,278 @@ class myThread implements Runnable {
 
 	}//end processOperation
 
+
+	 //---------------------------------
+    //	Create account
+    //---------------------------------
+	public static synchronized void CreateAcc(String [] reqSplit,  Connection con, OutputStream out)
+	{
+		 //check if account is already exit
+		Statement st = null;
+		String ecoMsg = "";
+	    boolean validAcc =true;
+	    int accN = Integer.parseInt(reqSplit[1]);
+		try
+		{
+			st = con.createStatement();
+			String sql ="SELECT accNum, balance FROM heq3456_BANKACCOUNTS";
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next())
+			{
+				//retrivel
+				int accNumber = rs.getInt("accNum");
+				if (accNumber == accN)
+				{	
+					validAcc = false;
+					break;
+				}
+			}
+			rs.close();
+		}catch(SQLException se){
+		se.printStackTrace();
+		}
+
+		//create account
+		if(validAcc)
+		{
+			try
+			{
+				st = con.createStatement();
+
+				String sql = "INSERT INTO heq3456_BANKACCOUNTS (accNum, balance) VALUES('"+ accN+"','"+ 0+"');";
+				st.executeUpdate(sql);
+				ecoMsg = "Creating Account " +accN + " Successfully. Current balance: 0.";
+				try{
+				out.write(ecoMsg.getBytes());
+				}catch(Exception e)
+				{
+					System.out.println("Write to Client failed.");
+				}
+
+			}catch(SQLException se){
+			se.printStackTrace();
+			}
+		}
+		else
+		{
+			ecoMsg = "Creation Failed, account number is already exist. ";
+			try{
+			out.write(ecoMsg.getBytes());
+			}catch(Exception e)
+			{
+				System.out.println("Write to Client failed.");
+			}
+		}
+
+	}// end Create AAcc
+
+
+	//---------------------------------
+	//         Retrive Account
+	//---------------------------------
+	public static synchronized void RetrivelAcc(String [] reqSplit, Connection con, OutputStream out)
+	{
+		boolean validAcc = false;
+		String ecoMsg = "";
+	    int accN = Integer.parseInt(reqSplit[1]);
+	    int amt = 0;
+		Statement st = null;
+		try
+		{
+			st = con.createStatement();
+			String sql ="SELECT accNum, balance FROM heq3456_BANKACCOUNTS";
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next())
+			{
+				//retrivel
+				int accNumber = rs.getInt("accNum");
+				if (accNumber == accN)
+				{	
+					amt = rs.getInt("balance");
+					validAcc = true;
+					break;
+				}
+			}
+			rs.close();
+		}catch(SQLException se){
+		se.printStackTrace();
+		}
+
+		if(validAcc)
+		{
+			ecoMsg = "Retriving account "+ accN+" successfully. Balance: "+ amt + " ";
+			try{
+		 	out.write(ecoMsg.getBytes());
+		 	}catch(Exception e)
+			{
+				System.out.println("Write to Client failed.");
+			}
+		}
+		else
+		{
+			ecoMsg = "Retriving failed. Account "+ accN + " does not existed.";
+			try{
+		 	out.write(ecoMsg.getBytes());
+		 	}catch(Exception e)
+			{
+				System.out.println("Write to Client failed.");
+			}	
+		}
+
+	}//end RetrivelAcc
+
+
+	//-----------------------------
+	//		Deposit to account
+	//------------------------------
+	public static synchronized void DepositToAcc(String [] reqSplit, Connection con, OutputStream out)
+	{
+		boolean validAcc = false;
+		String ecoMsg = "";
+	    int accN = Integer.parseInt(reqSplit[1]);
+	    int amtDposit = Integer.parseInt(reqSplit[2]);
+	  	int amt = 0;
+	    int accNumber;
+	    int newBalance = 0;
+		Statement st = null;
+		try
+		{
+			st = con.createStatement();
+			String sql ="SELECT accNum, balance FROM heq3456_BANKACCOUNTS";
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next())
+			{
+				//retrivel
+			    accNumber = rs.getInt("accNum");
+				if (accNumber == accN)
+				{	
+					amt = rs.getInt("balance");
+					validAcc = true;
+					break;
+				}
+			}
+			rs.close();
+		}catch(SQLException se){
+		se.printStackTrace();
+		}
+
+		if(validAcc)
+		{	
+			try
+			{
+				newBalance = amt + amtDposit;
+				String sql = "UPDATE heq3456_BANKACCOUNTS " + 
+							 " SET balance = ? WHERE accNum  = ?";
+				PreparedStatement preparedSt = con.prepareStatement(sql);
+				preparedSt.setInt(1, newBalance);
+				preparedSt.setInt(2, accN);
+				preparedSt.executeUpdate();
+			}catch(SQLException se){
+			se.printStackTrace();
+			}
+
+			ecoMsg = "Deposit to account "+ accN+" successfully. Now balance: "+ newBalance + ".";
+			try{
+		 	out.write(ecoMsg.getBytes());
+		 	}catch(Exception e)
+			{
+				System.out.println("Write to Client failed.");
+			}
+		}
+		else
+		{
+			ecoMsg = "Deposit to account failed. Account "+ accN + " does not existed.";
+			try{
+		 	out.write(ecoMsg.getBytes());
+		 	}catch(Exception e)
+			{
+				System.out.println("Write to Client failed.");
+			}
+		}
+	}// end DepositToAcc
+
+
+	//-----------------------------------------
+	//				WithDraw from account
+	//-------------------------------------------
+	public static synchronized void WdFromAcc(String [] reqSplit, Connection con, OutputStream out)
+	{
+		boolean validAcc = false;
+		String ecoMsg = "";
+	    int accN = Integer.parseInt(reqSplit[1]);
+	    int amtWd = Integer.parseInt(reqSplit[2]);
+	    int amt = 0;
+	    int accNumber;
+	    int newBalance = 0;
+		Statement st = null;
+		try
+		{
+			st = con.createStatement();
+			String sql ="SELECT accNum, balance FROM heq3456_BANKACCOUNTS";
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next())
+			{
+				//retrivel
+			    accNumber = rs.getInt("accNum");
+				if (accNumber == accN)
+				{	
+					amt = rs.getInt("balance");
+					validAcc = true;
+					break;
+				}
+			}
+			rs.close();
+		}catch(SQLException se){
+		se.printStackTrace();
+		}
+
+		if(validAcc)
+		{	
+			if(amtWd <= amt)
+			{
+				try
+				{
+					newBalance = amt - amtWd;
+					String sql = "UPDATE heq3456_BANKACCOUNTS " + 
+								 "SET balance = ? WHERE accNum =?";
+					PreparedStatement preparedSt = con.prepareStatement(sql);
+					preparedSt.setInt(1, newBalance);
+					preparedSt.setInt(2, accN);
+					preparedSt.executeUpdate();
+				}catch(SQLException se){
+				se.printStackTrace();
+				}
+
+				ecoMsg = "Withdraw from account "+ accN+" successfully. New balance: "+ newBalance + ".";
+				try{
+			 	out.write(ecoMsg.getBytes());
+			 	}catch(Exception e)
+				{
+					System.out.println("Write to Client failed.");
+				}
+			}
+			else
+			{
+				ecoMsg = "Withdraw from account failed. Account "+ accN + " does not have enough money. Current balance: "+amt;
+				try{
+			 	out.write(ecoMsg.getBytes());
+			 	}catch(Exception e)
+				{
+					System.out.println("Write to Client failed.");
+				}
+			}
+		}
+		else
+		{
+			ecoMsg = "Deposit to account failed. Account "+ accN + " does not existed.";
+			try{
+		 	out.write(ecoMsg.getBytes());
+		 	}catch(Exception e)
+			{
+				System.out.println("Write to Client failed.");
+			}
+		}
+	}// end DepositToAcc
+
 } // end class:SERVERCLASS
+
